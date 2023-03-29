787a6c0132d632954d9ccc5fef6672ae9aed345c
niklasb99 <niklas.burger@virtual-identity.com>
2023-03-29 16:43:57 +0200

    remove homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,29 +1,6 @@
 import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import HomepageFeatures from '@site/src/components/HomepageFeatures';
-import { useAppConfig } from '../hooks/useAppConfig';
-import styles from './index.module.css';
+import { Redirect } from '@docusaurus/router';
 
-function HomepageHeader({ appConfig }) {
-    return (
-        <header className={clsx('hero hero--primary', styles.heroBanner)}>
-            <div className="container">
-                <h1 className="hero__title">{appConfig.title}</h1>
-                <p className="hero__subtitle">{appConfig?.content?.claim}</p>
-            </div>
-        </header>
-    );
-}
 export default function Home() {
-    const { data } = useAppConfig();
-
-    return (
-        <Layout title={data.name}>
-            <HomepageHeader appConfig={data} />
-            <main>
-                <HomepageFeatures />
-            </main>
-        </Layout>
-    );
-}
+    return <Redirect to="/docs/docs/intro" />;
+  }
