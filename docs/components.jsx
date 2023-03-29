daa14ade734c4ee59e67268e65837942e26932ea
niklasb99 <niklas.burger@virtual-identity.com>
2023-03-29 14:38:39 +0200

    swap content from tsys with online consulting

---
+++ docs/components.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+  
+export const PrimaryNote = ({ children }) => (
+  <div
+    style={{
+      color: "black",
+      backgroundColor: "#FFF3E0",
+      padding: "16px",
+      marginBottom: "20px",
+      border: "1px solid #FF9F00",
+      borderRadius: "4px"
+    }}
+  >
+    <strong>&#9432;</strong> {children}
+  </div>
+);
+
